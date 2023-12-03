fa40efe6f19507b285896416d0d360a6d5b17244
Andy Schott <andy@andyschott.com>
2023-12-03 15:30:11 -0500

    refactor to add arguments so any day can be ran easily

---
+++ AdventOfCode/InputBinder.cs
@@ -0,0 +1,21 @@
+﻿using System.CommandLine;
+using System.CommandLine.Binding;
+
+namespace AdventOfCode;
+
+public class InputBinder : BinderBase<string>
+{
+    private readonly Argument<int> _dayArgument;
+
+    public InputBinder(Argument<int> dayArgument)
+    {
+        _dayArgument = dayArgument;
+    }
+
+    protected override string GetBoundValue(BindingContext bindingContext)
+    {
+        var day = bindingContext.ParseResult.GetValueForArgument(_dayArgument);
+
+        return PuzzleInput.GetInput(day);
+    }
+}
---
+++ AdventOfCode/PuzzleBinder.cs
@@ -0,0 +1,30 @@
+﻿using System.CommandLine;
+using System.CommandLine.Binding;
+
+namespace AdventOfCode;
+
+public class PuzzleBinder : BinderBase<IPuzzle>
+{
+    private readonly IReadOnlyDictionary<int, IPuzzle> _puzzles;
+    private readonly Argument<int> _dayArgument;
+
+    public PuzzleBinder(IEnumerable<IPuzzle> puzzles,
+        Argument<int> dayArgument)
+    {
+        _puzzles = puzzles.ToDictionary(puzzle => puzzle.Id);
+        _dayArgument = dayArgument;
+    }
+
+    protected override IPuzzle GetBoundValue(BindingContext bindingContext)
+    {
+        var day = bindingContext.ParseResult.GetValueForArgument(_dayArgument);
+        var puzzle = _puzzles.GetValueOrDefault(day);
+
+        if (puzzle is not null)
+        {
+            return puzzle;
+        }
+
+        throw new Exception($"Could not find puzzle with ID {day}");
+    }
+}
