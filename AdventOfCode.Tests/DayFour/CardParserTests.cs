9b9df0597118e7dcb9afc7726bbdc93383b13151
Andy Schott <andy@andyschott.com>
2023-12-04 18:24:15 -0500

    parse cards

---
+++ AdventOfCode.Tests/DayFour/CardParserTests.cs
@@ -0,0 +1,47 @@
+﻿using AdventOfCode.DayFour;
+using AdventOfCode.DayFour.Model;
+
+namespace AdventOfCode.Tests.DayFour;
+
+public class CardParserTests
+{
+    private readonly CardParser _parser = new();
+
+    [Theory]
+    [MemberData(nameof(ParseData))]
+    public void Parse(string input, Card expectedCard)
+    {
+        var card = _parser.ParseCard(input);
+
+        Assert.Equal(expectedCard, card);
+    }
+
+    public static TheoryData<string, Card> ParseData => new()
+    {
+        {
+            "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+            new Card(1, [ 41, 48, 83, 86, 17 ], [ 83, 86, 6, 31, 17, 9, 48, 53] )
+        },
+        {
+            "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+            new Card(2, [ 13, 32, 20, 16, 61], [ 61, 30, 68, 82, 17, 32, 24, 19])
+        },
+        {
+            "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+            new Card(3, [ 1, 21, 53, 59, 44 ], [ 69, 82, 63, 72, 16, 21, 14, 1])
+        },
+        {
+            "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+            new Card(4, [ 41, 92, 73, 84, 69 ], [59, 84, 76, 51, 58, 5, 54, 83 ] )
+        },
+        {
+            "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+            new Card(5, [ 87, 83, 26, 28, 32 ], [ 88, 30, 70, 12, 93, 22, 82, 36 ])
+        },
+        {
+
+            "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+            new Card(6, [ 31, 18, 13, 56, 72 ], [ 74, 77, 10, 23, 35, 67, 36, 11 ])
+        }
+    };
+}
---
+++ AdventOfCode/DayFour/CardParser.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+using AdventOfCode.DayFour.Model;
+
+namespace AdventOfCode.DayFour;
+
+public partial class CardParser
+{
+    private readonly Regex _cardRegex = CreateCardRegex();
+
+    public IEnumerable<Card> Parse(string input)
+    {
+        var lines = input.Split(Environment.NewLine);
+        return lines.Select(ParseCard)
+            .ToArray();
+    }
+
+    public Card ParseCard(string input)
+    {
+        var m = _cardRegex.Match(input);
+        var id = int.Parse(m.Groups["id"].Value);
+        var winningNumbers = ParseNumbers(m.Groups["winning"].Value);
+        var numbers = ParseNumbers(m.Groups["numbers"].Value);
+
+        return new Card(id, winningNumbers, numbers);
+    }
+
+    private static int[] ParseNumbers(string str)
+    {
+        return str.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(s => int.Parse(s))
+            .ToArray();
+    }
+
+    [GeneratedRegex(@"Card (?<id>\d+)\: (?<winning>.+) \| (?<numbers>.+)")]
+    private static partial Regex CreateCardRegex();
+}
